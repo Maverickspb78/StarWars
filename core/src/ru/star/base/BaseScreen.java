1cfce94840d709e89c152ad5ea978ba41113aacd
Maverickspb78 <Maverickspb78@gmail.com>
2021-02-22 14:20:57 +0300

    Урок 6(нормальный)

--- core/src/ru/star/base/BaseScreen.java
+++ core/src/ru/star/base/BaseScreen.java
@@ -17,7 +17,7 @@ public class BaseScreen implements Screen, InputProcessor {
 
     private Rect screenBounds;
     private Rect glBounds;
-    private Rect worldBounds;
+    protected Rect worldBounds;
 
     private Matrix4 worldToGl;
     private Matrix3 screenToWorld;
@@ -149,7 +149,6 @@ public class BaseScreen implements Screen, InputProcessor {
 
     @Override
     public boolean scrolled(float amountX, float amountY) {
-        System.out.println("scrolled amountX = " + amountX + " amountY = " + amountY);
         return false;
     }
 
--- core/src/ru/star/sprite/MainShip.java
+++ core/src/ru/star/sprite/MainShip.java
@@ -1,27 +1,19 @@
 package ru.star.sprite;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
-import ru.star.base.Sprite;
+import ru.star.base.Ship;
 import ru.star.math.Rect;
 import ru.star.pool.BulletPool;
 
-public class MainShip extends Sprite {
+public class MainShip extends Ship {
 
     private static final float HEIGHT = 0.15f;
     private static final float PADDING = 0.05f;
     private static final int INVALID_POINTER = -1;
-    private final Vector2 v0 = new Vector2(0.5f, 0);
-    private final Vector2 v = new Vector2();
-
-    private Rect worldBounds;
-    private BulletPool bulletPool;
-    private TextureRegion bulletRegion;
-    private Vector2 bulletV;
-    private Vector2 bulletPos;
 
     private boolean pressedLeft;
     private boolean pressedRight;
@@ -29,16 +21,19 @@ public class MainShip extends Sprite {
     private int leftPointer = INVALID_POINTER;
     private int rightPointer = INVALID_POINTER;
 
-    private float reloadInterval;
-    private float reloadTimer;
-
     public MainShip(TextureAtlas atlas, BulletPool bulletPool) {
         super(atlas.findRegion("main_ship"), 1, 2, 2);
         this.bulletPool = bulletPool;
         this.bulletRegion = atlas.findRegion("bulletMainShip");
+        v = new Vector2();
+        v0 = new Vector2(0.5f, 0);
         bulletV = new Vector2(0, 0.5f);
         bulletPos = new Vector2();
-        reloadInterval = 0.15f;
+        bulletHeight = 0.01f;
+        damage = 1;
+        reloadInterval = 0.2f;
+        sound = Gdx.audio.newSound(Gdx.files.internal("sounds/laser.wav"));
+        hp = 100;
     }
 
     @Override
@@ -47,14 +42,11 @@ public class MainShip extends Sprite {
         setBottom(worldBounds.getBottom() + PADDING);
         this.worldBounds = worldBounds;
     }
+
     @Override
     public void update(float delta) {
-        pos.mulAdd(v, delta);
-        reloadTimer += delta;
-        if (reloadTimer >= reloadInterval) {
-            reloadTimer = 0f;
-            shoot();
-        }
+        super.update(delta);
+        bulletPos.set(pos.x, pos.y + getHalfHeight());
         if (getRight() > worldBounds.getRight()) {
             setRight(worldBounds.getRight());
             stop();
@@ -64,6 +56,7 @@ public class MainShip extends Sprite {
             stop();
         }
     }
+
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
         if (touch.x < worldBounds.pos.x) {
@@ -81,6 +74,7 @@ public class MainShip extends Sprite {
         }
         return false;
     }
+
     @Override
     public boolean touchUp(Vector2 touch, int pointer, int button) {
         if (pointer == leftPointer) {
@@ -100,6 +94,7 @@ public class MainShip extends Sprite {
         }
         return false;
     }
+
     public boolean keyDown(int keycode) {
         switch (keycode) {
             case Input.Keys.A:
@@ -112,12 +107,10 @@ public class MainShip extends Sprite {
                 moveRight();
                 pressedRight = true;
                 break;
-            case Input.Keys.UP:
-                shoot();
-                break;
         }
         return false;
     }
+
     public boolean keyUp(int keycode) {
         switch (keycode) {
             case Input.Keys.A:
@@ -141,20 +134,21 @@ public class MainShip extends Sprite {
         }
         return false;
     }
+
+    public void dispose() {
+        sound.dispose();
+    }
+
     private void moveRight() {
         v.set(v0);
     }
+
     private void moveLeft() {
         v.set(v0).rotate(180);
     }
+
     private void stop() {
         v.setZero();
-    }
-
-    private void shoot() {
-        Bullet bullet = bulletPool.obtain();
-        bulletPos.set(pos.x, pos.y + getHalfHeight());
-        bullet.set(this, bulletRegion, bulletPos, bulletV, 0.01f, worldBounds, 1);
     }
 
 }
